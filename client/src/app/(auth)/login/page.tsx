08e6dc8667932b378fa10fe381d687dacc23bdcc
unknown <hieph@lotusoutsourcing.com>
2024-06-17 15:20:55 +0700

    video 12 server component next js

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -1,11 +1,24 @@
 'use client'
-import React, { useState } from "react";
+import React, { useState } from "react"
+import {websites } from '@/lib/data'
 
 export default function LoginPage() {
   const [email, setEmail] = useState("test@gmail.com")
   console.log('LoginPage')
   return <div>
     Login page
-    {email}
+    
+    <p>{email}</p>
+
+  <ul>
+    {websites.map((item) => {
+      return (
+        <li key={item.url}>
+          {item.name}
+        </li>
+      )
+    }
+    )}
+  </ul>
   </div>
 }
