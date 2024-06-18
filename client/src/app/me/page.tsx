3466f655f7f578d014a6407d5b945ad5cf19834d
unknown <hieph@lotusoutsourcing.com>
2024-06-18 17:37:53 +0700

    video_18_p1_authentication_next_js_44_minute

---
+++ client/src/app/me/page.tsx
@@ -0,0 +1,38 @@
+import envConfig from '@/config';
+import React from 'react'
+import { cookies } from 'next/headers'
+
+export default async function MeProfile() {
+  const cookieStore = cookies()
+  const sessionToken = cookieStore.get('sessionToken')
+  console.log(sessionToken)
+  const result = await fetch(
+    `${envConfig.NEXT_PUBLIC_API_ENDPOINT}/account/me`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        'Authorization': `Bearer ${sessionToken?.value}`
+      },
+      method: "GET",
+    }
+  ).then(async (res) => {
+    // console.log(res)
+    const payload = await res.json()
+    const data = {
+      status: res.status,
+      payload,
+    };
+    if (!res.ok) {
+      throw data
+    }
+    return data
+  
+  })
+  console.log(result)
+  return (
+    <div>
+      <h1>Profile</h1>
+      <div>Xin chào {result.payload.data?.name}</div>
+    </div>
+  )
+}
