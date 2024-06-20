00d4589705b230cd0f0d8663ebb2d097a2ccb215
unknown <hieph@lotusoutsourcing.com>
2024-06-20 10:17:15 +0700

    video_21_logout_next_js

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,21 +1,23 @@
-import { ModeToggle } from '@/components/mode-toogle'
-import Link from 'next/link'
-import React from 'react'
+import ButtonLogout from "@/components/button-logout"
+import { ModeToggle } from "@/components/mode-toogle"
+import Link from "next/link"
+import React from "react"
 
 export default function Header() {
   return (
     <div>
       <ul>
         <li>
-          <Link href='/login'>
-            Đăng nhập
-          </Link>
-          <Link href='/register'>
-            Đăng ký
-          </Link>
+          <Link href="/login">Đăng nhập</Link>
+        </li>
+        <li>
+          <Link href="/register">Đăng ký</Link>
+        </li>
+        <li>
+         <ButtonLogout/>
         </li>
       </ul>
-      <ModeToggle/>
+      <ModeToggle />
     </div>
-  )
+  );
 }
--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -30,3 +30,12 @@ export const handleErrorApi = ({error, setError, duration}: {
       })
     }
 }
+
+/**
+ *  Xóa đi ký tự `/` đầu tiên của path
+ * 
+ */
+
+export const normalizePath = (path: string) => {
+  return path.startsWith('/') ? path.slice(1) : path
+}
