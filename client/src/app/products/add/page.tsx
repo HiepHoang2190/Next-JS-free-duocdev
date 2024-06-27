35bb67a77654edece8c6420ffcb3c1c0155d0676
unknown <hieph@lotusoutsourcing.com>
2024-06-27 17:00:13 +0700

    video_26_upload_image_and_create_product

---
+++ client/src/app/products/add/page.tsx
@@ -0,0 +1,11 @@
+import ProductAddForm from "@/app/products/add/product-add-form";
+
+
+export default function ProductAddPage() {
+  return (
+    <div>
+      <h1>Thêm sản phẩm</h1>
+      <ProductAddForm />
+    </div>
+  )
+}
