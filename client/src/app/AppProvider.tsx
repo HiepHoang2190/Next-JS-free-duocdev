d0649b4ed1346922226b1724d8cea552399063cf
unknown <hieph@lotusoutsourcing.com>
2024-07-02 10:56:42 +0700

    video_33_share_global_state_user

--- client/src/app/AppProvider.tsx
+++ client/src/app/AppProvider.tsx
@@ -1,14 +1,39 @@
 "use client";
-import { useLayoutEffect, useState } from "react"
+import { useLayoutEffect } from "react"
 import { clientSessionToken } from "@/lib/http"
+import { AccountResType } from "@/schemaValidations/account.schema";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState
+} from 'react'
 
+type User = AccountResType['data']
+
+const AppContext = createContext<{
+  user: User | null
+  setUser: (user: User | null)=> void
+}>({
+  user: null,
+  setUser: () => {}
+})
+export const useAppContext = () => {
+  const context = useContext(AppContext)
+  return context
+}
 export default function AppProvider({
   children,
   inititalSessionToken = "",
+  user: userProp
 }: {
   children: React.ReactNode
-  inititalSessionToken?: string
+  inititalSessionToken?: string,
+  user: User | null
 }) {
+  // console.log(user)
+  const [user, setUser] = useState<User | null>(userProp)
   useState(()=>{
     if(typeof window !== 'undefined') {
       clientSessionToken.value = inititalSessionToken
@@ -19,9 +44,12 @@ export default function AppProvider({
   //   sessionToken.value = inititalSessionToken
   // }, [inititalSessionToken])
   return (
-  <>
+  <AppContext.Provider value={{
+    user,
+    setUser
+  }}>
     { children }
-  </>
+  </AppContext.Provider>
   )
 
 }
--- client/src/app/products/page.tsx
+++ client/src/app/products/page.tsx
@@ -4,20 +4,24 @@ import Image from "next/image";
 import React from "react";
 import { Button } from "@/components/ui/button";
 import DeleteProduct from "@/app/products/_components/delete-product";
+import { cookies } from "next/headers";
 
 export default async function ProductListPage() {
+  const cookieStore = cookies();
+  const sessionToken = cookieStore.get("sessionToken");
+  const isAuthenticated = Boolean(sessionToken);
   const { payload } = await productApiRequest.getList();
   const productList = payload.data;
   // console.log('productlist',productList)
   return (
     <div className="space-y-3">
+      {isAuthenticated && (
+        <Link href={"/products/add"}>
+          <Button variant={"secondary"}>Thêm sản phẩm</Button>
+        </Link>
+      )}
       <h1>Product List</h1>
-      <Link href={'/products/add'}>
-      <Button variant={'secondary'}>
-      Thêm sản phẩm
-      </Button>
-       
-      </Link>
+
       <div className="space-y-5">
         {productList.map((product) => (
           <div key={product.id} className="flex space-x-4">
@@ -33,13 +37,15 @@ export default async function ProductListPage() {
 
             <h3>{product.name}</h3>
             <div>{product.price}</div>
-            <div className="flex space-x-2">
-              <Link href={`/products/${product.id}`}>
-                <Button variant={"outline"}>Edit</Button>
-              </Link>
-              <DeleteProduct product={product}/>
-              
-            </div>
+            {isAuthenticated && (
+               <div className="flex space-x-2">
+               <Link href={`/products/${product.id}`}>
+                 <Button variant={"outline"}>Edit</Button>
+               </Link>
+               <DeleteProduct product={product} />
+             </div>
+            )}
+           
           </div>
         ))}
       </div>
